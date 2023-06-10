90f44a3af9c9359fa15c1d6554c45b596a148402
Andrew Garetov <andrew1gar@yandex.ru>
2023-06-11 02:02:48 +0300

    #1 Корректная авторизация

--- delivery/app/routes/api/handlers/UserSigninHandler.js
+++ delivery/app/routes/api/handlers/UserSigninHandler.js
@@ -2,8 +2,8 @@ const formatter = require('../../../utils/ResponseFormatter');
 const passport = require("passport");
 
 module.exports = async (req, res) => {
-  passport.authenticate('local', {}, (err, user) => {
-    if (!user) {
+  passport.authenticate('local')(req, res, () => {
+    if (!req.user) {
       return res
         .status(401)
         .json(formatter.error("Неверный логин или пароль"));
@@ -11,10 +11,10 @@ module.exports = async (req, res) => {
     return res
       .status(201)
       .json(formatter.ok({
-        id: user.id,
-        email: user.email,
-        name: user.name,
-        contactPhone: user.contactPhone,
+        id: req.user.id,
+        email: req.user.email,
+        name: req.user.name,
+        contactPhone: req.user.contactPhone,
       }));
-  })(req, res);
+  });
 };

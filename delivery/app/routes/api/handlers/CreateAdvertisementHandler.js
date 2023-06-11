8440b117ee7e402a75cc0cad560fcd9f179a5945
Andrew Garetov <andrew1gar@yandex.ru>
2023-06-11 21:01:48 +0300

    #1 Использование advertisementFormatter в CreateAdvertisementHandler

--- delivery/app/routes/api/handlers/CreateAdvertisementHandler.js
+++ delivery/app/routes/api/handlers/CreateAdvertisementHandler.js
@@ -1,6 +1,6 @@
 const formatter = require('../../../utils/ResponseFormatter');
+const advertisementFormatter = require('../../../utils/AdvertisementFormatter');
 const advertisementModule = require("../../../Modules/AdvertisementModule");
-const userModule = require("../../../Modules/UserModule");
 
 module.exports = async (req, res) => {
   try {
@@ -13,17 +13,7 @@ module.exports = async (req, res) => {
     const advertisement = await advertisementModule.create(params);
     return res
       .status(201)
-      .json(formatter.ok({
-        id: advertisement.id,
-        shortTitle: advertisement.shortTitle,
-        description: advertisement.description,
-        images: advertisement.images,
-        user: {
-          id: advertisement.userId,
-          name: (await userModule.findById(advertisement.userId)).name,
-        },
-        createdAt: advertisement.createdAt,
-      }));
+      .json(formatter.ok(advertisementFormatter.format(advertisement)));
   } catch (e) {
     const error = e.message || "Ошибка при сохранении объявления";
     return res

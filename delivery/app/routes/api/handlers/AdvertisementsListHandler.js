aea704b55c7d72ff55d7f28c70f220b02a025323
Andrew Garetov <andrew1gar@yandex.ru>
2023-06-11 21:03:05 +0300

    #1 Обработчик для получения списка объявлений

---
+++ delivery/app/routes/api/handlers/AdvertisementsListHandler.js
@@ -0,0 +1,19 @@
+const formatter = require('../../../utils/ResponseFormatter');
+const advertisementFormatter = require('../../../utils/AdvertisementFormatter');
+const advertisementModule = require("../../../Modules/AdvertisementModule");
+
+module.exports = async (req, res) => {
+  try {
+    const advertisements = await advertisementModule.list();
+    return res
+      .status(200)
+      .json(formatter.ok(await Promise.all(advertisements.map(async (advertisement) => {
+        return await advertisementFormatter.format(advertisement)
+      }))));
+  } catch (e) {
+    const error = e.message || "Ошибка при получении списка объявлений";
+    return res
+      .status(401)
+      .json(formatter.error(error));
+  }
+};

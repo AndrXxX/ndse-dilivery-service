a07442566d874c3c154e92ade4264e75dd6b803d
Andrew Garetov <andrew1gar@yandex.ru>
2023-06-13 23:31:43 +0300

    #1 Добавлен MessageFormatter

---
+++ delivery/app/utils/MessageFormatter.js
@@ -0,0 +1,12 @@
+module.exports = {
+  async format(message) {
+    return {
+      author: typeof message.author === "object" ? {
+        id: message.author.id,
+        name: message.author.name,
+      } : { id: message.author },
+      sentAt: message.sentAt,
+      text: message.text,
+    };
+  },
+};
